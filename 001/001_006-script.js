c9d05574a34ec783168793ff26ac1404b602f49f
OlegBon <oleh.bon@yahoo.com>
2025-07-10 12:18:18 +0300

    add task 1.6

---
+++ 001/001_006-script.js
@@ -0,0 +1,4 @@
+let pi = Math.PI; // Отримуємо число π
+let outputDiv = document.querySelector(".output");
+
+outputDiv.textContent = pi.toFixed(3); // Форматуємо число до трьох знаків після коми і виводимо його на екран
