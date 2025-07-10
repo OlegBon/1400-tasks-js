06383fa2d5ab6a00e55719f581cc5ee88bc3a8a1
OlegBon <oleh.bon@yahoo.com>
2025-07-10 12:22:05 +0300

    add task 1.7

---
+++ 001/001_007-script.js
@@ -0,0 +1,4 @@
+let e = Math.E; // Отримуємо число e (основу натурального логарифма)
+let outputDiv = document.querySelector(".output");
+
+outputDiv.textContent = e.toFixed(1); // Форматуємо число до одного знака після коми і виводимо його на екран
